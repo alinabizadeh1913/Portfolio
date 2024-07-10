374c465a223a4b5b9bfcecbdb83edb2baed1956d
ali nabizadeh <alinabizadeh1913@gmail.com>
2024-07-11 00:56:03 +0330

    My Portfolio

---
+++ src/js/main.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const goToSocialMedia = document.querySelector('.go-to-contact button');
+const linkWrapper = document.querySelector('.social-media .links');
+const contactBtn = document.querySelector('.contact-btn');
+const contactWrapper = document.querySelector('.contact-me');
+const contact = document.querySelector('.contact-me .contact');
+const close = document.querySelector('.contact-me .close i');
+const darkMode = document.querySelector('.dark-mode');
+const darkModebtn = document.querySelector('.dark-mode i');
+
+goToSocialMedia.addEventListener('click', () => {
+    linkWrapper.classList.toggle('show-links');
+});
+
+contactBtn.addEventListener('click',() => {
+    contactWrapper.style.display = 'flex';
+    contact.style.display = 'block';
+    document.body.style.overflow = 'hidden'
+    setTimeout(() => {
+        contactWrapper.style.visibility = 'visible';
+        contactWrapper.style.opacity = 1;
+        contact.style.visibility = 'visible';
+        contact.style.opacity = 1;
+    },100)
+})
+
+close.addEventListener('click',() => {
+    contactWrapper.style.visibility = 'hidden';
+    contactWrapper.style.opacity = 0;
+    contact.style.visibility = 'hidden';
+    contact.style.opacity = 0;
+    document.body.style.overflow = 'auto'
+    setTimeout(() => {
+        contactWrapper.style.display = 'none';
+        contact.style.display = 'none';
+    },300)
+})
+
+contactWrapper.addEventListener('click',(e) => {
+    if(e.target.className.includes('contact-me')){
+        contactWrapper.style.visibility = 'hidden';
+        contactWrapper.style.opacity = 0;
+        contact.style.visibility = 'hidden';
+        contact.style.opacity = 0;
+        document.body.style.overflow = 'auto'
+        setTimeout(() => {
+            contactWrapper.style.display = 'none';
+            contact.style.display = 'none';
+        },300)
+    }
+})
+
+
+let isDark = true;
+
+darkMode.addEventListener('click', () => {
+    isDark = !isDark;
+    if(isDark){
+        document.documentElement.style.setProperty('--background','#5464de')
+        document.documentElement.style.setProperty('--circle','#1f4b6df5')
+        document.documentElement.style.setProperty('--links','#0e4375')
+        document.documentElement.style.setProperty('--links-hover','#59d5ff')
+
+        darkModebtn.classList.remove('fa-moon')
+        darkModebtn.classList.add('fa-sun')
+    }else{
+        document.documentElement.style.setProperty('--background','#0f172a')
+        document.documentElement.style.setProperty('--circle','#1f414c')
+        document.documentElement.style.setProperty('--links','#35798f')
+        document.documentElement.style.setProperty('--links-hover','#abe1f3')
+        darkModebtn.classList.remove('fa-sun')
+        darkModebtn.classList.add('fa-moon')
+    }
+})
